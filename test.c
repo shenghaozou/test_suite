554f2f5f0bb7ef23cd21ee8431d9773140c302a4
szou28 <szou28@node.imgfs.imgfs-pg0.wisc.cloudlab.us>
2019-07-02 08:57:44 -0500

    update test

--- test.c
+++ test.c
@@ -4,68 +4,96 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <time.h>
 
-#define LAYERS 2
-#define FILES_EACH_LAYER 1000
+#define LAYERS 10
+#define FILES_EACH_LAYER 2000
 #define MIN_SIZE 1000
 #define MAX_SIZE 2000
-#define REPEAT 100
+#define REPEAT 30
 
 char path[128];
 
-int test_layer(int layer) {
+double get_wall_time(){
+    struct timeval time;
+    if (gettimeofday(&time,NULL)){
+        printf("ERROR when fetching current wall time.\n");
+		exit(1);
+        return 0;
+    }
+    return (double)time.tv_sec + (double)time.tv_usec * .000001;
+}
+
+double test_layer(int layer) {
   struct stat st;
-  clock_t before = clock();
+  double before = get_wall_time();
   for (int j = 0; j < FILES_EACH_LAYER; j++) {
      sprintf(path, "./%d-%d.txt", layer, j);
      int rc = stat(path, &st);
      assert(rc == 0);
   }
-  clock_t difference = clock() - before;
-  return difference * 1000 / CLOCKS_PER_SEC;
+  return get_wall_time() - before;
 }
 
-void print_layers_time_cost(int msec[], int sum) {
+void print_layers_time_cost(double msec[], double sum) {
   int i;
   for (int i = 0; i < LAYERS; i++) {
-    printf("Layer %d: %d ms\n", i, msec[i]);
+    printf("Layer %d: %f s\n", i, msec[i]);
   }
-  printf("\nTotal: %d ms (%d stat tests).\n", sum, LAYERS * FILES_EACH_LAYER);
+  printf("\nTotal: %f s (%d stat tests).\n", sum, LAYERS * FILES_EACH_LAYER);
 }
 
-int test_layers(int msec[]) {
-  int sum = 0;
+double test_layers(double msec[]) {
+  double sum = 0;
   for (int i = 0; i < LAYERS; i++) {
     msec[i] = test_layer(i);
     sum += msec[i];
   }
-  //print_layers_time_cost(msec, sum);
   return sum;
 }
 
-int test_layers_rev(int msec[]) {
-  int sum = 0;
+double test_layers_rev(double msec[]) {
+  double sum = 0;
   for (int i = LAYERS - 1; i >= 0; i--) {
     msec[i] = test_layer(i);
     sum += msec[i];
   }
-  //print_layers_time_cost(msec, sum);
   return sum;
+}
+
+int get_repeat_times(int argc, char *argv[]) {
+  printf("repeat mode\n");
+  if (argc >= 3) {
+    int rt = atoi(argv[2]);
+    printf("the default repeat times has been changed to %d\n", rt);
+    return rt;
+  }
+  else {
+    return REPEAT;
+  }
 }
 
 int main(int argc, char *argv[]) {
     int i, j;
     struct stat st;
-    int msec[LAYERS];
-    int sum = 0;
+    double msec[LAYERS];
+    double sum = 0;
+
+    double total_msec[LAYERS];
+    double total_sum = 0;
+
+    int repeat_times = REPEAT;
 
     srand(time(0));
 
     if (strcmp(argv[1], "new") == 0) {
         printf("creating new test files.\n");
-
+        char debug = 0;
+        if (argc >= 3 && strcmp(argv[2], "debug") == 0) {
+          debug = 1;
+        }
         for (i = 0; i < LAYERS; i++) {
             sprintf(path, "./layer-%d", i);
             if (stat(path, &st) == -1) {
@@ -76,12 +104,18 @@ int main(int argc, char *argv[]) {
         char random_data[MAX_SIZE + 1];
         for (i = 0; i < LAYERS; i++) {
             for (j = 0; j < FILES_EACH_LAYER; j++) {
-                sprintf(path, "./layer-%d/%d-%d.txt", i, i, j);
-                printf("file: %s", path);
+                if (debug) {
+                  sprintf(path, "./%d-%d.txt", i, j);
+                }
+                else {
+                  sprintf(path, "./layer-%d/%d-%d.txt", i, i, j);
+                }
+                //printf("file: %s\n", path);
                 FILE *fp;
                 fp = fopen(path, "w");
                 if (fp == NULL) {
-                    printf("ERROR. CANNOT OPEN THE FILE.\n");
+                    printf("ERROR. CANNOT OPEN THE FILE: %s\n", path);
+                    exit(1);
                 }
                 else {
                     int sz = rand() % (MAX_SIZE - MIN_SIZE + 1) + MIN_SIZE;
@@ -92,6 +126,7 @@ int main(int argc, char *argv[]) {
                     random_data[sz] = 0;
                     fprintf(fp, "%s", random_data);
                 }
+                fclose(fp);
             }
         }
     }
@@ -104,17 +139,28 @@ int main(int argc, char *argv[]) {
       print_layers_time_cost(msec, sum);
     }
     else if (strcmp(argv[1], "test_repeat") == 0) {
-      int total_msec[LAYERS];
-      int total_sum = 0;
+      repeat_times = get_repeat_times(argc, argv);
       memset(total_msec, 0, sizeof(total_msec));
-
-      for (i = 0; i < REPEAT; i++) {
+      for (i = 0; i < repeat_times; i++) {
         test_layers(msec);
         for (j = 0; j < LAYERS; j++) {
           total_msec[j] += msec[j];
         }
       }
-
+      for (j = 0; j < LAYERS; j++) {
+        total_sum += total_msec[j];
+      }
+      print_layers_time_cost(total_msec, total_sum);
+    }
+    else if (strcmp(argv[1], "test_repeat_rev") == 0) {
+      repeat_times = get_repeat_times(argc, argv);
+      memset(total_msec, 0, sizeof(total_msec));
+      for (i = 0; i < repeat_times; i++) {
+        test_layers(msec);
+        for (j = LAYERS - 1; j >= 0; j--) {
+          total_msec[j] += msec[j];
+        }
+      }
       for (j = 0; j < LAYERS; j++) {
         total_sum += total_msec[j];
       }
